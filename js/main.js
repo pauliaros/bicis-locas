043eacd0091735c632c7f0daefa33cbb2b102b0e
Paulina Aros <aros.pau@gmail.com>
2017-05-15 14:02:37 -0400

    ejercicio evaluado bicis

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var password = document.getElementById("input-password").value;
+	var selector = document.getElementsByClassName("form-control")[4].value;
+	var twitter = document.getElementById("input-social").value;
+
+	var nuevoNodoNombre = document.createElement("span");
+	var nuevoTextoNombre = document.createTextNode("Ingrese su nombre, la primera letra debe ser en mayuscula");
+	var nombreIngresado = document.getElementsByClassName("name-container")[0];
+		nuevoNodoNombre.appendChild(nuevoTextoNombre);
+		nombreIngresado.appendChild(nuevoNodoNombre);
+
+	var nuevoNodoApellido = document.createElement("span");
+	var nuevoTextoApellido = document.createTextNode("Ingrese su apellido, la primera letra debe ser en mayuscula");
+	var apellidoIngresado = document.getElementsByClassName("lastname-container")[0];
+		nuevoNodoApellido.appendChild(nuevoTextoApellido);
+		apellidoIngresado.appendChild(nuevoNodoApellido);
+
+	var nuevoNodoEmail = document.createElement("span");
+	var nuevoTextoEmail = document.createTextNode("Ingrese su correo electrónico");
+	var EmailIngresado = document.getElementsByClassName("email-container")[0];
+		nuevoNodoEmail.appendChild(nuevoTextoEmail);
+		EmailIngresado.appendChild(nuevoNodoEmail);	
+	
+	nombre = document.getElementById("name").value;
+if( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre) ) {
+  return false;
 }
+
+
